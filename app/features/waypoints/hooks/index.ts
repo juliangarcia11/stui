4201de243eeaee418e37eae5831415fad781c649
Julian <jlmurraygarcia@gmail.com>
2026-03-11 05:49:43 -0700

    feat(waypoints): add useWaypointsData to get loader data

--- app/features/waypoints/hooks/index.ts
+++ app/features/waypoints/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from "./useWaypointDialog";
+export * from "./useWaypointsData";
---
+++ app/features/waypoints/hooks/useWaypointsData.ts
@@ -0,0 +1,15 @@
+import { useLoaderData } from "react-router";
+import { type LoadWaypointsDataResponse } from "../loader-waypoints";
+
+/**
+ * Custom hook to access the data loaded by the Waypoints route's loader function.
+ *
+ * @returns The data loaded by the loader function, including:
+ *   - agent information,
+ *   - system information
+ *   - a list of waypoints
+ */
+export function useWaypointsData() {
+  const data = useLoaderData<LoadWaypointsDataResponse>();
+  return data;
+}
