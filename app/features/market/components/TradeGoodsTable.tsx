b4a4a27c7d35e53bce4184e774224b419795423c
Julian <jlmurraygarcia@gmail.com>
2026-03-13 06:10:46 -0700

    feat(market): update TradeGoodsTable to use UITradeGood type & data

--- app/features/market/components/TradeGoodsTable.tsx
+++ app/features/market/components/TradeGoodsTable.tsx
@@ -2,13 +2,13 @@
 import { Table } from "@radix-ui/themes";
 import type { FC } from "react";
 import { extractSchemaDescriptions } from "~/api";
-import type { MarketTradeGood } from "~/api/client";
 import {
   ActivityLevelSchema,
   MarketTradeGoodSchema,
   SupplyLevelSchema,
 } from "~/api/client/schemas.gen";
 import { TextWithHelp } from "~/components";
+import type { UITradeGood } from "../types";
 
 const HELP = {
   ...extractSchemaDescriptions(MarketTradeGoodSchema),
@@ -20,14 +20,14 @@ const HELP = {
   }),
 };
 
-export const TradeGoodsTable: FC<{ goods?: MarketTradeGood[] }> = ({
+export const TradeGoodsTable: FC<{ goods?: UITradeGood[] }> = ({
   goods = [],
 }) => {
   return (
     <Table.Root variant="surface" size="1">
       <Table.Header>
         <Table.Row>
-          <Table.ColumnHeaderCell>Symbol</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Good</Table.ColumnHeaderCell>
           <Table.ColumnHeaderCell>Type</Table.ColumnHeaderCell>
           <Table.ColumnHeaderCell>
             <TextWithHelp text="Trade Volume" helpText={HELP.tradeVolume} />
@@ -50,7 +50,9 @@ export const TradeGoodsTable: FC<{ goods?: MarketTradeGood[] }> = ({
       <Table.Body>
         {goods.map((good) => (
           <Table.Row key={good.symbol}>
-            <Table.RowHeaderCell>{good.symbol}</Table.RowHeaderCell>
+            <Table.RowHeaderCell>
+              <TextWithHelp text={good.name} helpText={good.description} />
+            </Table.RowHeaderCell>
             <Table.Cell>{good.type}</Table.Cell>
             <Table.Cell>{good.tradeVolume}</Table.Cell>
             <Table.Cell>{good.supply}</Table.Cell>
